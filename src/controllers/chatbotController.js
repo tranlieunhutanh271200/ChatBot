5287dea5c5312e481ea563bd2334f9c4f7543350
anhtln@vnvc.vn <anhtln@vnvc.vn>
2023-08-01 10:55:53 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -60,26 +60,65 @@ let postWebhook = (req, res) => {
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
-
     let response;
-  
-    // Check if the message contains text
-    if (received_message.text) {    
-  
-      // Create the payload for a basic text message
-      response = {
-        "text": `You sent the message: "${received_message.text}". Now send me an image!`
-      }
-    }  
     
-    // Sends the response message
+    // Checks if the message contains text
+    if (received_message.text) {    
+      // Create the payload for a basic text message, which
+      // will be added to the body of our request to the Send API
+      response = {
+        "text": `"${received_message.text}" Cái nồn ton tặc!`
+      }
+    } else if (received_message.attachments) {
+      // Get the URL of the message attachment
+      let attachment_url = received_message.attachments[0].payload.url;
+      response = {
+        "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "generic",
+            "elements": [{
+              "title": "Đây có phải Sứa mặt thộn không?",
+              "subtitle": "Nhấn nút ở dưới để trả lời.",
+              "image_url": attachment_url,
+              "buttons": [
+                {
+                  "type": "postback",
+                  "title": "Có!",
+                  "payload": "yes",
+                },
+                {
+                  "type": "postback",
+                  "title": "Không!",
+                  "payload": "no",
+                }
+              ],
+            }]
+          }
+        }
+      }
+    } 
+    
+    // Send the response message
     callSendAPI(sender_psid, response);    
   }
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
-
-}
+    let response;
+    
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+  
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+      response = { "text": "OK! Chào Sứa gà con chip chip." }
+    } else if (payload === 'no') {
+      response = { "text": "Oops, try sending another image." }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
+  }
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
