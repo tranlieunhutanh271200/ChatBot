afc647e32ffb53af123cbfb3ddc5474f448e7ef8
anhtln@vnvc.vn <anhtln@vnvc.vn>
2023-08-01 15:26:12 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -115,6 +115,8 @@ function handlePostback(sender_psid, received_postback) {
         response = { "text": "OK! Chào Sứa gà con chip chip." }
     } else if (payload === 'no') {
         response = { "text": "Oops, try sending another image." }
+    } else if (payload === 'GET_STARTED') {
+        response = { "text": "Chào mừng bạn đến với Nhựt Anh channel" }
     }
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
