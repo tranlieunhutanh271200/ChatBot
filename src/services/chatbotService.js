abc0f2930b57d3b62afc1004f1570b2ff0e46c38
anhtln@vnvc.vn <anhtln@vnvc.vn>
2023-08-01 16:37:49 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -48,7 +48,7 @@ let getUserName = (sender_psid) => {
         }, (err, res, body) => {
             if (!err) {
                 body = JSON.parse(body);
-                let username = `${response.first_name} ${response.last_name}`
+                let username = `${response.last_name} ${response.first_name}`
                 resolve(username);
             } else {
                 console.error("Unable to send message:" + err);
