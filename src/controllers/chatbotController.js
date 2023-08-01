5fe90254d869c2f0b4a2bce39b1889b646cfeccc
anhtln@vnvc.vn <anhtln@vnvc.vn>
2023-08-01 10:45:00 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+import request from "request";
+
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
@@ -8,49 +10,71 @@ let getHomePage = (req, res) => {
 
 let getWebhook = (req, res) => {
 
-  let mode = req.query["hub.mode"];
-  let token = req.query["hub.verify_token"];
-  let challenge = req.query["hub.challenge"];
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
 
-  // Check if a token and mode is in the query string of the request
-  if (mode && token) {
-    // Check the mode and token sent is correct
-    if (mode === "subscribe" && token === VERIFY_TOKEN) {
-      // Respond with the challenge token from the request
-      res.status(200).send(challenge);
-    } else {
-      // Respond with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
+    // Check if a token and mode is in the query string of the request
+    if (mode && token) {
+        // Check the mode and token sent is correct
+        if (mode === "subscribe" && token === VERIFY_TOKEN) {
+            // Respond with the challenge token from the request
+            res.status(200).send(challenge);
+        } else {
+            // Respond with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
     }
-  }
 
 };
 
 let postWebhook = (req, res) => {
     let body = req.body;
     if (body.object === "page") {
-        body.entry.forEach(function(entry) {
+        body.entry.forEach(function (entry) {
 
             // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
-          
+
+
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
-          
-          });
-          res.status(200).send('EVENT_RECEIVED');
-      } else {
+
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
+
+        });
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
         // Return a '404 Not Found' if event is not from a page subscription
         res.sendStatus(404);
-      }
+    }
 };
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
 
-}
+    let response;
+  
+    // Check if the message contains text
+    if (received_message.text) {    
+  
+      // Create the payload for a basic text message
+      response = {
+        "text": `You sent the message: "${received_message.text}". Now send me an image!`
+      }
+    }  
+    
+    // Sends the response message
+    callSendAPI(sender_psid, response);    
+  }
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
@@ -59,12 +83,32 @@ function handlePostback(sender_psid, received_postback) {
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+      "recipient": {
+        "id": sender_psid
+      },
+      "message": response
+    }
   
-}
+    // Send the HTTP request to the Messenger Platform
+    request({
+      "uri": "https://graph.facebook.com/v2.6/me/messages",
+      "qs": { "access_token": PAGE_ACCESS_TOKEN },
+      "method": "POST",
+      "json": request_body
+    }, (err, res, body) => {
+      if (!err) {
+        console.log('message sent!')
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }); 
+  }
 
 
 module.exports = {
-    getHomePage : getHomePage,
-    getWebhook : getWebhook,
-    postWebhook : postWebhook
+    getHomePage: getHomePage,
+    getWebhook: getWebhook,
+    postWebhook: postWebhook
 }
