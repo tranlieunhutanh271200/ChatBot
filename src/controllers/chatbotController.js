d8b242caf684dfde93ff7c3df4e59e842ad38b6e
anhtln@vnvc.vn <anhtln@vnvc.vn>
2023-08-01 14:08:03 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -61,93 +61,115 @@ let postWebhook = (req, res) => {
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
     let response;
-    
+
     // Checks if the message contains text
-    if (received_message.text) {    
-      // Create the payload for a basic text message, which
-      // will be added to the body of our request to the Send API
-      response = {
-        "text": `"${received_message.text}" Cái nồn ton tặc!`
-      }
-    } else if (received_message.attachments) {
-      // Get the URL of the message attachment
-      let attachment_url = received_message.attachments[0].payload.url;
-      response = {
-        "attachment": {
-          "type": "template",
-          "payload": {
-            "template_type": "generic",
-            "elements": [{
-              "title": "Đây có phải Sứa mặt thộn không?",
-              "subtitle": "Nhấn nút ở dưới để trả lời.",
-              "image_url": attachment_url,
-              "buttons": [
-                {
-                  "type": "postback",
-                  "title": "Có!",
-                  "payload": "yes",
-                },
-                {
-                  "type": "postback",
-                  "title": "Không!",
-                  "payload": "no",
-                }
-              ],
-            }]
-          }
+    if (received_message.text) {
+        // Create the payload for a basic text message, which
+        // will be added to the body of our request to the Send API
+        response = {
+            "text": `"${received_message.text}" Cái nồn ton tặc!`
         }
-      }
-    } 
-    
+    } else if (received_message.attachments) {
+        // Get the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Đây có phải Sứa mặt thộn không?",
+                        "subtitle": "Nhấn nút ở dưới để trả lời.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Có!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "Không!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
+        }
+    }
+
     // Send the response message
-    callSendAPI(sender_psid, response);    
-  }
+    callSendAPI(sender_psid, response);
+}
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
     let response;
-    
+
     // Get the payload for the postback
     let payload = received_postback.payload;
-  
+
     // Set the response based on the postback payload
     if (payload === 'yes') {
-      response = { "text": "OK! Chào Sứa gà con chip chip." }
+        response = { "text": "OK! Chào Sứa gà con chip chip." }
     } else if (payload === 'no') {
-      response = { "text": "Oops, try sending another image." }
+        response = { "text": "Oops, try sending another image." }
     }
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
-  }
+}
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
     // Construct the message body
     let request_body = {
-      "recipient": {
-        "id": sender_psid
-      },
-      "message": response
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
     }
-  
+
     // Send the HTTP request to the Messenger Platform
     request({
-      "uri": "https://graph.facebook.com/v2.6/me/messages",
-      "qs": { "access_token": PAGE_ACCESS_TOKEN },
-      "method": "POST",
-      "json": request_body
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
     }, (err, res, body) => {
-      if (!err) {
-        console.log('message sent!')
-      } else {
-        console.error("Unable to send message:" + err);
-      }
-    }); 
-  }
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
+let setupProfile = (req, res) => {
+    // call profile facebook api
+    // Construct the message body
+    let request_body = {
+        "get_started" : { "payload": "GET_STARTED" },
+        "whitelisted_domains": ["https://chatbot-nhutanh.onrender.com/"]
+    }
 
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": `https://graph.facebook.com/v17.0/me/messenger_profile?access_token=${PAGE_ACCESS_TOKEN}`,
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
 
 module.exports = {
     getHomePage: getHomePage,
     getWebhook: getWebhook,
-    postWebhook: postWebhook
+    postWebhook: postWebhook,
+    setupProfile: setupProfile
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -5,6 +5,8 @@ let router = express.Router();
 
 let initWebRouter = (app) => {
     router.get('/', chatbotController.getHomePage);
+
+    router.post('/setup-profile', chatbotController.setupProfile);
     router.get('/webhook', chatbotController.getWebhook);
     router.post('/webhook', chatbotController.postWebhook);
     return app.use("/", router);
