f630873eb8d6c3558edaaa7d55d8bdb341748e87
anhtln@vnvc.vn <anhtln@vnvc.vn>
2023-08-01 10:31:30 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
-
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 let getHomePage = (req, res) => {
     return res.send("Hello");
 };
 
 let getWebhook = (req, res) => {
-  let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
   let mode = req.query["hub.mode"];
   let token = req.query["hub.verify_token"];
   let challenge = req.query["hub.challenge"];
@@ -28,13 +29,39 @@ let getWebhook = (req, res) => {
 let postWebhook = (req, res) => {
     let body = req.body;
     if (body.object === "page") {
-        // Returns a '200 OK' response to all requests
-        res.status(200).send("EVENT_RECEIVED");
+        body.entry.forEach(function(entry) {
+
+            // Gets the body of the webhook event
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+          
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+          
+          });
+          res.status(200).send('EVENT_RECEIVED');
       } else {
         // Return a '404 Not Found' if event is not from a page subscription
         res.sendStatus(404);
       }
 };
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
+}
+
 
 module.exports = {
     getHomePage : getHomePage,
